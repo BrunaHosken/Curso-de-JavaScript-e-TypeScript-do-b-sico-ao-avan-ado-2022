42a7e2931d631a3a21ee54336e32a187b817951d
Bruna De Martins Hosken <bruna.hosken@a4pm.com.br>
2025-01-12 14:53:20 -0300

    criando index global

---
+++ global/routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const route = express.Router();
+const homeController = require("../mod11_projetoagenda/src/controllers/homeController");
+const loginController = require("../mod11_projetoagenda/src/controllers/loginController");
+const path = require("path");
+
+// Rotas da agenda
+route.get("/agenda", homeController.index);
+route.get("/agenda/login/index", loginController.index);
+
+// Rota para a página inicial global
+route.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../global/html/index.html"));
+});
+
+// Rota para a landing page
+route.get("/landing_page", (req, res) => {
+  res.sendFile(path.join(__dirname, "../landing_page/index.html"));
+});
+
+module.exports = route;
---
+++ global/server.js
@@ -0,0 +1,97 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const routes = require("./routes");
+const path = require("path");
+const helmet = require("helmet");
+const csrf = require("csurf");
+const {
+  middlewareGlobal,
+  checkCsrfError,
+  csrfMiddleware,
+} = require("../mod11_projetoagenda/src/middlewares/middleware");
+
+// Conexão com o banco de dados
+mongoose
+  .connect(process.env.CONNECTIONSTRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.emit("pronto");
+  })
+  .catch((e) => console.log(e));
+
+// Segurança com Helmet
+
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        imgSrc: ["'self'", "data:", "https:"], // Permite imagens locais e externas
+        styleSrc: ["'self'", "'unsafe-inline'", "https:"], // Permite CSS inline e externo
+        scriptSrc: ["'self'", "'unsafe-inline'", "https:"], // Permite scripts locais e externos
+        fontSrc: ["'self'", "https:", "data:"], // Permite fontes externas
+      },
+    },
+  })
+);
+
+// Configuração para receber dados do corpo da requisição
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Configuração de arquivos estáticos
+app.use(express.static(path.join(__dirname, "html")));
+
+app.use(
+  "/landing_page",
+
+  express.static(path.resolve(__dirname, "../landing_page"))
+);
+app.use(
+  "/agenda/public",
+  express.static(path.resolve(__dirname, "../mod11_projetoagenda/public"))
+);
+
+// Configuração de sessões
+const sessionOptions = session({
+  secret: "akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()",
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+  },
+});
+app.use(sessionOptions);
+app.use(flash());
+
+// Configuração das views
+app.set("views", path.resolve(__dirname, "../mod11_projetoagenda/src/views"));
+app.set("view engine", "ejs");
+
+// Configuração de CSRF
+app.use(csrf());
+app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
+
+// Rotas
+app.use(routes);
+
+// Inicialização do servidor
+app.on("pronto", () => {
+  app.listen(3000, () => {
+    console.log(path.resolve(__dirname, "../mod11_projetoagenda/public"));
+
+    console.log("Acessar http://localhost:3000");
+    console.log("Servidor executando na porta 3000");
+  });
+});
