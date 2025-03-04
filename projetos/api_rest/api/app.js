ed698228ba1fafe54681b81f0f4dd139c9865269
Bruna De Martins Hosken <bruna.hosken@a4pm.com.br>
2025-03-04 14:34:17 -0300

    adiciando cors e helmet

--- projetos/api_rest/api/app.js
+++ projetos/api_rest/api/app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
 const homeRoutes = require("../src/routes/homeRoutes");
 const alunoRoutes = require("../src/routes/alunoRoutes");
 const userRoutes = require("../src/routes/userRoutes");
@@ -6,6 +9,18 @@ const tokenRoutes = require("../src/routes/tokenRoutes");
 const fotoRoutes = require("../src/routes/fotoRoutes");
 
 require("../src/database");
+
+const whiteList = ["http://35.247.229.37", "http://localhost:3000"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -15,6 +30,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
   }
